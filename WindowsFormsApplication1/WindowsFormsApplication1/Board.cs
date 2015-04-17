26c84d79f0e21f0d4e9ab11f13f492640b9ed436
gordonmj <neologue@gmail.com>
2015-04-17 17:55:43 -0400

    Display board with dots

--- WindowsFormsApplication1/WindowsFormsApplication1/Board.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Board.cs
@@ -28,13 +28,18 @@ namespace WindowsFormsApplication1
             System.Drawing.Graphics pG = p.CreateGraphics();
             int width = (p.Width-1)/8;
             int height = (p.Height-1)/8;
-            Pen border = new Pen(new SolidBrush(Color.Black), 10);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            Pen border = new Pen(blackBrush, 10);
             for (int r = 0; r < 8; r++)
             {
                 for (int c = 0; c < 8; c++)
                 {
-                    //pG.FillRectangle(new SolidBrush(Color.Green), width * c, height * r, width, height);
-                    pG.DrawRectangle(new Pen(new SolidBrush(Color.Black)), width * c, height * r, width, height);
+                    //dots go at 2,2 6,2 2,6 6,6
+                    pG.DrawRectangle(new Pen(blackBrush), width * c, height * r, width, height);
+                    if ((r==2 || r==6) && (c==2 || c==6))
+                    {
+                        pG.FillEllipse(blackBrush, (width * c) - 4, (height * r) - 4, 10, 10);
+                    }
                 }
             }
         }
